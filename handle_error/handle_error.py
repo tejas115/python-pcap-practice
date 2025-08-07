76ff9dfcd806945a1cf81dfc5c66a425d622fc7f
tejas.shah1 <tejas.b.shah.ctr@army.mil>
2025-08-07 16:11:26 -0500

    handle exceptions

---
+++ handle_error/handle_error.py
@@ -0,0 +1,22 @@
+def division():
+    global number1, number2
+    number1 = input("Enter a number: ")
+    number2 = input("Enter another number: ")
+
+
+#rewrite the code so that all if/elif/else statements are replaced by a try/except code block. Use two named exceptions and one unnamed.
+    try:
+        print(float(number1) / float(number2))
+    except ZeroDivisionError as zde:
+        print("Zero division error, please try again.", zde)
+        division()
+    except ValueError as ve:
+        print("Invalid input. Please enter numeric values.", ve )
+        division()
+    except Exception as e:
+        print("something went wrong, try again:", e)
+    division()
+
+
+
+division()
