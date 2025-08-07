41e17f83c87303f498cbdf392d09898fc1eaa675
tejas.shah1 <tejas.b.shah.ctr@army.mil>
2025-08-07 16:20:52 -0500

    fixed the recursive call error

--- handle_error/handle_error.py
+++ handle_error/handle_error.py
@@ -7,15 +7,16 @@ def division():
 #rewrite the code so that all if/elif/else statements are replaced by a try/except code block. Use two named exceptions and one unnamed.
     try:
         print(float(number1) / float(number2))
+        return
     except ZeroDivisionError as zde:
         print("Zero division error, please try again.", zde)
-        division()
+        division() # retry
     except ValueError as ve:
         print("Invalid input. Please enter numeric values.", ve )
-        division()
+        division() # retry
     except Exception as e:
         print("something went wrong, try again:", e)
-    division()
+        division()  # retry
 
 
 
