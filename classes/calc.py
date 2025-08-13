a2312853a08013a09f7422d022da0eaf55257d88
tejas.shah1 <tejas.b.shah.ctr@army.mil>
2025-08-13 17:48:02 -0500

    understanding self

--- classes/calc.py
+++ classes/calc.py
@@ -18,13 +18,22 @@ class Computer:
         # Add configuration logic here
         print(f"Configuration: {self.cpu}, {self.ram}")
 
+    def compare_memory(self, other):
+        if self.ram > other.ram:
+            print(f"{self.brand} {self.model} has more RAM than {other.brand} {other.model}")
+        elif self.ram < other.ram:
+            print(f"{self.brand} {self.model} has less RAM than {other.brand} {other.model}")
+        else:
+            print(f"{self.brand} {self.model} has the same amount of RAM as {other.brand} {other.model}")
 
 com1 = Computer("Dell", "XPS 13", "i5", "16GB RAM")
 com1.display_info()
-com1.config()   
+com1.config()
+print(id(com1))
 
 com2 = Computer("Apple", "MacBook Pro", "M1", "8GB RAM")
 com2.display_info()
 com2.config()
+print(id(com2))
 
-
+com1.compare_memory(com2)
