b5816ac063630ca1435f108382aa766d5e1abc60
tejas.shah1 <tejas.b.shah.ctr@army.mil>
2025-08-20 17:46:00 -0500

    user input

---
+++ user_input/sys_argv.py
@@ -0,0 +1,7 @@
+import sys
+
+x = int(sys.argv[1])  # Get the first command line argument
+y = int(sys.argv[2])  # Get the second command line argument
+
+z = x + y  # Add the two arguments
+print(f"The result is: {z}")  # Print the result
---
+++ user_input/user_input.py
@@ -0,0 +1,23 @@
+# Get user input and add
+x = int(input("Enter something: "))
+print(type(x))
+y = int(input("Enter something else: "))
+print(type(y))
+# a = int(x)
+# b = int(y)
+
+z = x + y
+
+print(z)
+
+# get input character
+char = input("Enter a character: ")[0]  # Get the first character
+print(type(char))
+print(char)
+
+
+#
+result = eval(input('enter an expression: '))
+print(result)
+
+
