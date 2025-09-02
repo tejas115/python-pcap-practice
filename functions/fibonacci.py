2acd8405d9540d9ff9d9687a66a08b0bf15c0fd9
tejas.shah1 <tejas.b.shah.ctr@army.mil>
2025-09-02 16:11:54 -0500

    fibonacci

---
+++ functions/fibonacci.py
@@ -0,0 +1,21 @@
+# fibonnaci series
+def fibonacci(n):
+    a, b = 0, 1
+    if n < 1:
+        print("you entered a negative number")
+        return
+    elif n == 1:
+        print(a, end=" ")
+    else:
+        print(a, b, end=" ")
+        for i in range(2, n):
+            c = a + b
+            a = b
+            b = c
+            print(c, end=" ")
+
+
+n = int(input("Enter a number: "))
+fibonacci(n)
+print()
+
