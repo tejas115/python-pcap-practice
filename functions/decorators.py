86f444ed6fd4ff9f7a41166f4857265984b0e872
tejas.shah1 <tejas.b.shah.ctr@army.mil>
2025-09-02 17:43:45 -0500

    decorators

---
+++ functions/decorators.py
@@ -0,0 +1,19 @@
+def div(a, b):
+    if a < b:
+        a, b = b, a
+    print(a / b)
+
+def smart_div(func):
+    def inner(a, b):
+        if a > b:
+            a, b = b, a
+        return func(a, b)
+    return inner
+
+
+
+div(4, 2)
+div(2, 4)
+
+div = smart_div(div)
+div(2, 4)
