8a57145815a74d587d348bbfc2dea59466ba58a4
tejas.shah1 <tejas.b.shah.ctr@army.mil>
2025-09-02 16:47:23 -0500

    recursion limits

---
+++ functions/recursion_limits.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.setrecursionlimit(1500)
+print(sys.getrecursionlimit())
+
+i = 0
+
+def greet():
+    global i
+    i += 1
+    print("Hello! Welcome to the program.", i)
+    greet()
+
+
+greet()
