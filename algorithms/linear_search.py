3995a4fd4337cb207c1a1b2aa9db196a2b0573aa
tejas.shah1 <tejas.b.shah.ctr@army.mil>
2025-09-04 17:33:04 -0500

    linear search

---
+++ algorithms/linear_search.py
@@ -0,0 +1,19 @@
+pos = -1
+
+def search(list, n):
+    i = 0
+    while i < len(list):
+        if list[i] == n:
+            globals()['pos'] = i
+            return True
+        i += 1
+
+    return False
+
+list = [5, 8, 4, 6, 9, 2]
+n = 9
+
+if search(list, n):
+    print(f"Found {n} at position {pos+1}")
+else:
+    print(f"{n} not found")
